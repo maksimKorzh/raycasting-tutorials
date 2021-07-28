b12f2c0e88c2347f63327be24f06e53e6e9c3c19
Maksim Korzh <freesoft.for.people@gmail.com>
2021-07-28 15:40:42 +0300

    working on 2D version

--- src/pygame-raycasting/raycasting.py
+++ src/pygame-raycasting/raycasting.py
@@ -1,27 +1,128 @@
-import sys, pygame
+# packages
+import sys
+import pygame
+import math
+
+# init pygame
 pygame.init()
+clock = pygame.time.Clock()
 
-win = pygame.display.set_mode((500,500))
-pygame.display.set_caption("First Game")
+# map (MAP_SIZE x MAP_SIZE)
+MAP = (
+    '########'
+    '#  #   #'
+    '#  ##  #'
+    '#      #'
+    '###  # #'
+    '#    # #'
+    '#    # #'
+    '########'
+)
 
-player_x = 50
-player_y = 50
+# global constants
+SCREEN_HEIGHT = 500
+SCREEN_WIDTH = SCREEN_HEIGHT * 2
+CASTED_RAYS = 10
+MAP_SIZE = 8
+TILE_SIZE = SCREEN_HEIGHT / MAP_SIZE
+MAX_DEPTH = int(MAP_SIZE * TILE_SIZE)
+FIELD_OF_VIEW = math.pi / 3
+STEP_ANGLE = FIELD_OF_VIEW / CASTED_RAYS
 
-while True:
-    pygame.time.delay(50)
+# global variables
+player_x = SCREEN_HEIGHT / 2
+player_y = SCREEN_HEIGHT / 2
+player_angle = 0.0
+
+# create window
+win = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("Ray casting")
+
+# draw map
+def draw_map():
+    # draw map background
+    pygame.draw.rect(win, (50, 50, 50), (0, 0, SCREEN_HEIGHT, SCREEN_HEIGHT))
+    
+    # loop over map rows
+    for row in range(8):
+        # loop over map colums
+        for col in range(8):
+            # convert row & col ti map square index
+            square = row * MAP_SIZE + col
+            
+            # draw rectangle if find 'wall' at map square index
+            pygame.draw.rect(win, (200, 200, 200) if MAP[square] == '#' else (100, 100, 100),
+                            (col * TILE_SIZE,
+                             row * TILE_SIZE,
+                             TILE_SIZE-2,
+                             TILE_SIZE-2))
+        
+    # draw player
+    pygame.draw.circle(win, (255, 0, 0), (int(player_x), int(player_y)), 8)
+
+# ray casting algorithm
+def cast_rays():
+    # define initial angle
+    start_angle = player_angle - FIELD_OF_VIEW / 2
+    
+    # loop over casting rays (used screen width pixels)
+    for ray in range(CASTED_RAYS):
+        # cast ray
+        for depth in range(MAX_DEPTH):
+            # extend ray
+            target_x = player_x + depth * math.sin(start_angle)
+            target_y = player_y + depth * math.cos(start_angle)
+            
+            # ray hits the wall
+            if MAP[int(target_y / TILE_SIZE) * MAP_SIZE + int(target_x / TILE_SIZE)] == '#':
+                # highlight the wall
+                target_col = int(target_x / TILE_SIZE)
+                target_row = int(target_y / TILE_SIZE)
+                pygame.draw.rect(win, (0, 255, 0), (target_col * TILE_SIZE,
+                                                    target_row * TILE_SIZE,
+                                                    TILE_SIZE - 2,
+                                                    TILE_SIZE - 2))
+                # drop casting ray
+                break
+            
+            # draw casting ray
+            pygame.draw.line(win, (255, 255, 0), (player_x, player_y), (target_x, target_y))
+            
+            
+            
+            
+        
+        # increment angle
+        start_angle += STEP_ANGLE
+    
+
+# game loop
+while True:    
+    # escpare condition
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit(0)
-
+    
+    # draw 2D map
+    draw_map()
+    
+    # render scene
+    cast_rays()
+            
+    # get user input
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_LEFT]: player_x -= 5
-    if keys[pygame.K_RIGHT]: player_x += 5
+    # update player position/view
+    if keys[pygame.K_a]: player_angle += 0.1
+    if keys[pygame.K_d]: player_angle -= 0.1
     if keys[pygame.K_UP]: player_y -= 5
     if keys[pygame.K_DOWN]: player_y += 5
+    if keys[pygame.K_LEFT]: player_x -= 5
+    if keys[pygame.K_RIGHT]: player_x += 5
     
-    win.fill((50,50,50))  # Fills the screen with black
-    pygame.draw.rect(win, (0,255,0), (player_x, player_y, 5, 5))   
-    pygame.display.update() 
-    
+    # update frame according to FPS
+    pygame.display.flip()
+    clock.tick(30)
+
+# clean ups
 pygame.quit()
